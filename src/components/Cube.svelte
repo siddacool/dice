7cc8b5d45f09e5b9b6464ba27cca562436839bbe
Siddhesh Mangela <sidthecool007@gmail.com>
2019-08-23 14:06:26 +0530

    faces added on dice cube

--- src/components/Cube.svelte
+++ src/components/Cube.svelte
@@ -1,8 +1,9 @@
 <script>
-  let number = 1;
+  const randomFace = () => Math.floor(Math.random() * 6 + 1);
+  let number = randomFace();
 
   function rollDice() {
-    number = Math.floor(Math.random() * 6 + 1);
+    number = randomFace();
   }
 
   const faces = {
@@ -44,7 +45,7 @@
     text-align: center;
     position: absolute;
     background: rgb(231,76,60);
-    background: radial-gradient(circle, rgba(231,76,60,1) 0%, rgba(252,133,121,.7) 100%);
+    background: radial-gradient(circle, rgba(231,76,60,1) 0%, rgba(252,133,121,.8) 100%);
     border-radius: 10px;
     color: #fff;
   }
@@ -72,15 +73,170 @@
   .bottom {
     transform: rotateX(-90deg) translateZ(100px);
   }
+
+  .dot {
+    width: 50px;
+    height: 50px;
+    background: #fff;
+    display: inline-block;
+    border-radius: 50%;
+  }
+
+  .face-inner {
+    position: relative;
+    display: flex;
+    padding: 10px;
+    height: 100%;
+  }
+
+  /* Face 1 */
+  .front .face-inner {
+    justify-content: center;
+    align-items: center;
+  }
+
+  /* Face 2 */
+  .top .face-inner .dot:nth-child(2) {
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
+
+  /* Face 3 */
+  .left .face-inner .dot:nth-child(2) {
+    position: absolute;
+    top: calc(50% - 25px);
+    left: calc(50% - 25px);
+  }
+
+  .left .face-inner .dot:nth-child(3) {
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
+
+  /* Face 4 */
+  .right .face-inner .dot:nth-child(2) {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+
+  .right .face-inner .dot:nth-child(3) {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+  }
+
+  .right .face-inner .dot:nth-child(4) {
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
+
+  /* Face 5 */
+  .bottom .face-inner .dot:nth-child(2) {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+
+  .bottom .face-inner .dot:nth-child(3) {
+    position: absolute;
+    top: calc(50% - 25px);
+    left: calc(50% - 25px);
+  }
+
+  .bottom .face-inner .dot:nth-child(4) {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+  }
+
+  .bottom .face-inner .dot:nth-child(5) {
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
+
+  /* Face 6 */
+  .back .face-inner .dot:nth-child(2) {
+    position: absolute;
+    top: 10px;
+    left: calc(50% - 25px);
+  }
+
+  .back .face-inner .dot:nth-child(3) {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+
+  .back .face-inner .dot:nth-child(4) {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+  }
+
+  .back .face-inner .dot:nth-child(5) {
+    position: absolute;
+    left: calc(50% - 25px);
+    bottom: 10px;
+  }
+
+  .back .face-inner .dot:nth-child(6) {
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
 </style>
 
 <div class="scene">
   <div class="cube" on:click={rollDice} style={`transform: translateZ(-100px) ${faces[number.toString()]};`}>
-    <div class="face front">1</div>
-    <div class="face back">6</div>
-    <div class="face right">4</div>
-    <div class="face left">3</div>
-    <div class="face top">2</div>
-    <div class="face bottom">5</div>
+    <div class="face front">
+      <div class="face-inner">
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="face back">
+      <div class="face-inner">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="face right">
+      <div class="face-inner">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="face left">
+      <div class="face-inner">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="face top">
+      <div class="face-inner">
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div class="face bottom">
+      <div class="face-inner">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
   </div>
 </div>
